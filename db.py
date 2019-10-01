f5346ebbb584033fc17c809068d61d7e1ee420e5
mssorokine <54817057+mssorokine@users.noreply.github.com>
2019-10-01 15:07:06 +0300

    Create db.py

---
+++ db.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+import settings
+
+db = MongoClient(settings.MONGO_LINK)[settings.MONGO_DB]
+
+def get_or_create_user(db, message):
+    user = db.users.find_one({"user_id": message.from_user.id})
+    if not user:
+        user = {
+            "user_id": message.from_user.id,
+            "first_name": message.from_user.first_name,
+            "last_name": message.from_user.last_name,
+            "username": message.from_user.username,
+            "books_count": 0,
+            "chat_id": message.chat_id,
+            "books": [{
+                "name": "",
+                "author": "",
+                "start_date": "",
+                "read_by": "",
+                "favorite": "",
+                "in_progress": "",
+                "days_count": 0
+            }]
+        }
+        db.users.insert_one(user)
+    return user
